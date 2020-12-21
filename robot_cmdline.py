a0af69d358abfc336e1d7bb3a80fdbe31d29bc9b
Jeremy Hyde <jeremy.hyde@latch.com>
2020-12-21 02:45:07 -0500

    lo

--- robot_cmdline.py
+++ robot_cmdline.py
@@ -68,21 +68,21 @@ def main():
     robot_logger.info('User setup complete!')
 
     while True:
-        if not CNC_MOTION:
-            user_option = user_input(user_logger).upper()
-            robot.queue_command(user_option)
-
-        if user_option == 'CNC_MOTION':
-            CNC_MOTION = not CNC_MOTION
-
         if CNC_MOTION:
             cnc_option = cnc_motion_input(user_logger)
             robot.queue_command(user_option, cnc_option)
+        else:
+            user_option = user_input(user_logger).upper()
 
-        if user_option == 'CNC_FEEDRATE':
-            cnc_option = cnc_feedrate_input(user_logger)
-            robot.queue_command(user_option, cnc_option)
 
+            if user_option == 'CNC_MOTION':
+                CNC_MOTION = not CNC_MOTION
+
+            elif user_option == 'CNC_FEEDRATE':
+                cnc_option = cnc_feedrate_input(user_logger)
+                robot.queue_command(user_option, cnc_option)
+            else:
+                robot.queue_command(user_option)
         #else:
         #    robot.queue_command(user_option)
 
