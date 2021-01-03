76b3c696757aba9a14187a66b08935e946da377d
Jeremy Hyde <jeremy.hyde@latch.com>
2021-01-03 02:10:45 -0500

    gui

--- gui/gui-test.py
+++ gui/gui-test.py
@@ -23,14 +23,15 @@ class Plant_GUI(tk.Frame):
     def main_window(self):
         self.main_window = tk.Canvas(master = self.master, width = WIDTH, height=HEIGHT, bg='black')
 
+        self.border = int(WIDTH*0.05)
         # Camera box
-        self.camera_pos = [15, 100]
-        self.c_dim = [int(WIDTH*0.5), int(WIDTH*0.35)]
+        self.camera_pos = [self.border, 100]
+        self.c_dim = [int(WIDTH*0.4), int(WIDTH*0.35)]
         self.draw_rect(self.camera_pos, self.c_dim, 'darkgrey')
 
         # Lighting box
-        self.lighting_pos = [int(WIDTH*0.55), 50]
-        self.l_dim = [int(WIDTH*0.4), 110]
+        self.lighting_pos = [int(WIDTH*0.5), 50]
+        self.l_dim = [int(WIDTH*0.45), 110]
         self.draw_rect(self.lighting_pos, self.l_dim, 'darkgrey')
         self.draw_rect((self.lighting_pos[0]+5, self.lighting_pos[1]-5), self.l_dim, 'darkgrey')
 
