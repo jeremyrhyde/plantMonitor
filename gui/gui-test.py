72b3305740cc701298fe52fe1f8b27b954e6afb7
Jeremy Hyde <jeremy.hyde@latch.com>
2021-01-03 03:54:41 -0500

    gui

--- gui/gui-test.py
+++ gui/gui-test.py
@@ -44,7 +44,7 @@ class Plant_GUI(tk.Frame):
 
         # Movement box
         self.movement_pos = [self.lighting_pos[0], self.y_mid2]
-        self.m_dim = [self.l_dim[0], HEIGHT - (self.y_mid2 + 2*self.border)]
+        self.m_dim = [self.l_dim[0], HEIGHT - (self.y_mid2 + 3*self.border)]
         self.draw_rect(self.movement_pos, self.m_dim, 'darkgrey')
         self.draw_rect((self.movement_pos[0]+5, self.movement_pos[1]-5), self.m_dim, 'darkgrey')
         self.movement_inputs()
