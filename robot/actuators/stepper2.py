14c654af2a036ac233cc35d7caae2d15853e997c
Jeremy Hyde <jeremy.hyde@latch.com>
2021-02-07 15:43:22 -0500

    remove cnc temp

--- robot/actuators/stepper2.py
+++ robot/actuators/stepper2.py
@@ -19,14 +19,14 @@ class Stepper:
     # Alternatively specify a different address and/or bus:
     # pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
 
-    def __init__(self, step_pin = 6, dir_pin = 5, enable_pin = 19, limit_switch_pin = 26, res_pins = (24,23,22), step_size = 'Full'):
-        self.res_pins = res_pins#(14,15,18)
+    def __init__(self, step_pin = 6, dir_pin = 5, enable_pin = 19, limit_switch_pin = 26):#, res_pins = (24,23,22), step_size = 'Full'):
+        #self.res_pins = res_pins#(14,15,18)
         self.step_pin = step_pin#21
         self.dir_pin = dir_pin#20
         self.enable_pin = enable_pin#16
         self.limit_switch_pin = limit_switch_pin
 
-        self.step_size = step_size
+        #self.step_size = step_size
 
         self.motor_init_time = 0.05
         self.motor_step_delay = 0.00125
@@ -38,7 +38,7 @@ class Stepper:
         GPIO.setup(self.dir_pin, GPIO.OUT)
         GPIO.setup(self.step_pin, GPIO.OUT)
 
-        GPIO.output(self.enable_pin, GPIO.HIGH)
+        self._disableDriver()
 
         self.motor = None#RpiMotorLib.A4988Nema(self.dir_pin, self.step_pin, self.res_pins, 'A4988')
         self.curr_pos = 0
@@ -50,65 +50,16 @@ class Stepper:
             self.switch1 = Limit_Switch_Sensor(self.limit_switch_pin)
 
     def _enableDriver(self):
-        GPIO.output(self.enable_pin, False)
+        GPIO.output(self.enable_pin, GPIO.LOW)
 
     def _disableDriver(self):
-        GPIO.output(self.enable_pin, True)
-
-    def vary_stepsize(self, stepsize):
-        self.stepsize = stepsize
-
-    def convert_degree_to_rev(self, delta_d):
-
-        ang = abs(delta_d)
-
-        if delta_d > 0:
-            dir = False
-        else:
-            dir = True
-
-        if self.step_size == 'Full':
-            d = 1
-        elif self.step_size == 'Half':
-            d = 1
-        elif self.step_size == '1/4':
-            d = 4
-        elif self.step_size == '1/8':
-            d = 8
-
-        rev = int(ang*200*d/360)
-
-        return dir, rev
-
-    def calculate_motion(self, desired_pos):
-        delta_d = desired_pos - self.curr_pos
-
-        #print('From: ' + str(self.curr_pos) + '    To: ' + str(desired_pos))
-
-        dir, rev = self.convert_degree_to_rev(delta_d)
-
-        if dir:
-            self.total_rev = self.total_rev + rev
-        else:
-            self.total_rev = self.total_rev - rev
-
-        #print(' --- Direction: ' + str(dir) + '    Revolutions: ' + str(rev))
-        return dir, rev
-
-    def move_stepper(self, desired_pos):
-
-        dir, rev = self.calculate_motion(desired_pos)
-
-        self._enableDriver()
-        #print('Rev: {} Desired_pos: {} curr_pos: {} total_rev: {}'.format(rev, desired_pos, self.curr_pos, self.total_rev))
-        self.motor.motor_go(dir, self.step_size, rev, self.motor_step_delay, False , self.motor_init_time)
-
-        self.curr_pos = desired_pos
+        GPIO.output(self.enable_pin, GPIO.HIGH)
 
 
-    def move_stepper2(self):
+    def move_stepper2(self, disable = True):
 
-        GPIO.output(self.enable_pin, GPIO.LOW)
+        if disable: self._enableDriver()
+
         i = 0
         while i < 200:
             GPIO.output(self.step_pin, GPIO.HIGH)
@@ -116,7 +67,7 @@ class Stepper:
             GPIO.output(self.step_pin, GPIO.LOW)
             i = i + 1
 
-        GPIO.output(self.enable_pin, GPIO.HIGH)
+        if disable: self._disableDriver()
 
     def calibration(self):
 
@@ -138,7 +89,7 @@ class Stepper:
             GPIO.output(self.step_pin, False)
             time.sleep(self.motor_step_delay)
 
-        self.curr_pos = 0 #degrees
+        #self.curr_pos = 0 #degrees
 
     def release_motor(self):
         GPIO.output(self.enable_pin, True)
