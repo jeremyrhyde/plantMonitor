7befb804338670fb44fa557635bb95fb84d42f4a
Jeremy Hyde <jeremy.hyde@latch.com>
2021-02-10 19:29:18 -0500

    sch

---
+++ gardener_cmdline.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+This application provides a CLI for the robots
+"""
+
+from robot import Robot_Scheduler
+from robot.Robot import Robot
+from robot.robot_config import *
+
+from subprocess import Popen, PIPE
+from util import Logger
+import time
+
+
+# ---- Types of manual input ----
+def user_input(logger):
+    print("\nOptions: On / Off / X")
+
+    user_option = input("Enter option: ")
+
+    logger.info("User input - {}".format(user_option))
+
+    return user_option
+
+def main():
+
+    gardener_id = '000'
+
+    # Logging
+    logger = Logger('temp.log')
+    gardener_logger = logger.init('GARDENER', gardener_id)
+
+    gardener_logger.info('Gardener setup complete!')
+
+
+    # Clean up
+    print("Exiting program...")
+    gardener_logger.close()
+
+
+if __name__ == "__main__":
+    main()
