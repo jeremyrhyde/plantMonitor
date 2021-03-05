3f03f7ccded0bde33f743fe190176e2970d4b18d
Jeremy Hyde <jeremy.hyde@latch.com>
2021-03-04 19:19:26 -0500

    plant dict

--- overseer/plant_dict.py
+++ overseer/plant_dict.py
@@ -1,7 +1,7 @@
 plant_dict = {
     'Rosemary':         {'scientific_name': 'Salvia rosmarinus',   'type': 'herb',       'state': 'plant',  'position': '%(60,100)',  'water_schedule': ('week' ,1),  'water_amount': 8,  'present': 'yes'},
-    'Gold-Leaf-Thyme':  {'scientific_name': 'Thymus citriodorus',  'type': 'herb',       'state': 'plant',  'position': '%(75,50)',   'water_schedule': ('week' ,1),  'water_amount': 8,  'present': 'yes'},
-    'Sage':             {'scientific_name': 'Salvia officinalis',  'type': 'herb',       'state': 'plant',  'position': '%(60,0)',    'water_schedule': ('week' ,2),  'water_amount': 0,  'present': 'yes'},
+    'Gold-Leaf-Thyme':  {'scientific_name': 'Thymus citriodorus',  'type': 'herb',       'state': 'plant',  'position': '%(70,50)',   'water_schedule': ('week' ,1),  'water_amount': 8,  'present': 'yes'},
+    'Sage':             {'scientific_name': 'Salvia officinalis',  'type': 'herb',       'state': 'plant',  'position': '%(60,0)',    'water_schedule': ('week' ,2),  'water_amount': 5,  'present': 'yes'},
     'Shishito-Peppers': {'scientific_name': 'Capsicum annuum',     'type': 'pepper',     'state': 'seed',   'position': '%(50,50)',   'water_schedule': ('day'  ,1),  'water_amount': 5,  'present': 'yes'},
     'Jade-Plant':       {'scientific_name': 'Crassula ovata',      'type': 'succulent',  'state': 'plant',  'position': '%(100,50)',  'water_schedule': ('week' ,2),  'water_amount': 5,  'present': 'yes'},
     'Ponthos':          {'scientific_name': 'Epipremnum aureum',   'type': 'plant',      'state': 'plant',  'position': '%(0,54)',    'water_schedule': ('day'  ,1),  'water_amount': 8,  'present': 'yes'},
