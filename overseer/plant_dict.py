ce5aa609c2b0b72edc4e52d5d2f7980856d5111d
Jeremy Hyde <jeremy.hyde@latch.com>
2021-04-05 00:21:49 -0400

    plant pos update

--- overseer/plant_dict.py
+++ overseer/plant_dict.py
@@ -7,7 +7,7 @@ plant_dict = {
     'Ponthos':          {'scientific_name': 'Epipremnum aureum',   'type': 'plant',      'state': 'plant',  'position': '%(100,100)',    'water_schedule': ('day'  ,1),  'water_amount': 8,  'present': 'yes'},
     'Chives':           {'scientific_name': 'Epipremnum aureum',   'type': 'plant',      'state': 'plant',  'position': '[%(35,0-100), %(40,100)]',    'water_schedule': ('day'  ,1),  'water_amount': 8,  'present': 'yes'},
     'Lavendar':         {'scientific_name': 'Epipremnum aureum',   'type': 'plant',      'state': 'plant',  'position': '[%(20,0-100), %(24,0-100)]',    'water_schedule': ('day'  ,1),  'water_amount': 8,  'present': 'yes'},
-    'Parsley':          {'scientific_name': 'Epipremnum aureum',   'type': 'plant',      'state': 'plant',  'position': '[%(9,0-100), %(5,0-100)],    'water_schedule': ('day'  ,1),  'water_amount': 8,  'present': 'yes'},
+    'Parsley':          {'scientific_name': 'Epipremnum aureum',   'type': 'plant',      'state': 'plant',  'position': '[%(9,0-100), %(5,0-100)]',    'water_schedule': ('day'  ,1),  'water_amount': 8,  'present': 'yes'},
     'Basil':            {'scientific_name': 'Epipremnum aureum',   'type': 'plant',      'state': 'plant',  'position': '%(55,0)',    'water_schedule': ('day'  ,1),  'water_amount': 8,  'present': 'yes'},
     'Spearmint':        {'scientific_name': 'Epipremnum aureum',   'type': 'plant',      'state': 'plant',  'position': '%(55,100)',    'water_schedule': ('day'  ,1),  'water_amount': 8,  'present': 'yes'},
 }
