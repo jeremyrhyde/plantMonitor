fedecee813fb4c4dae8eefd835b09351ebc6e9d4
Jeremy Hyde <jeremy.hyde@latch.com>
2021-08-21 19:24:53 -0400

    improts

--- robot/actuators/stepper.py
+++ robot/actuators/stepper.py
@@ -108,8 +108,8 @@ def main():
             stepper.release_motor()
         elif user_input == 'C':
             stepper.calibration()
-        elif user_input.isdigit():
-            elif user_input.isdigit() or (user_input[0] == '-' and user_input[1:].isdigit()):
+        elif user_input.isdigit() or (user_input[0] == '-' and user_input[1:].isdigit()):
+            stepper.move_stepper(int(user_input))
         elif user_input[0] == 'S':
             stepper._set_step_delay(float(user_input[1:]))
         else:
