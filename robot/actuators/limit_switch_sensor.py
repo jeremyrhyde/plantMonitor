e985beb030d2c080597bb7df4c92afd3705971f7
Jeremy Hyde <jeremy.hyde@latch.com>
2021-08-21 19:32:16 -0400

    improts

--- robot/actuators/limit_switch_sensor.py
+++ robot/actuators/limit_switch_sensor.py
@@ -44,7 +44,7 @@ class Limit_Switch_Sensor:
     def _output_state(self):
         while not self._stop_event.is_set():
             self._state  = GPIO.input(self._LIMIT_SWITCH_PIN)
-            print(self._state)
+            if verbose: print(self._state)
             time.sleep(0.1)
 
     def close(self):
