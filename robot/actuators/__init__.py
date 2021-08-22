e24951c359f6323719de0002032a986987faa466
Jeremy Hyde <jeremy.hyde@latch.com>
2021-08-21 20:00:28 -0400

    improts

--- robot/actuators/__init__.py
+++ robot/actuators/__init__.py
@@ -1,2 +1,3 @@
 from .cnc import CNC_Controller
 from .limit_switch_sensor import Limit_Switch_Sensor
+from .stepper import Stepper
--- robot/actuators/cnc.py
+++ robot/actuators/cnc.py
@@ -5,7 +5,7 @@
 import RPi.GPIO as GPIO
 import time
 
-from .stepper import Stepper
+from stepper import Stepper
 
 class CNC_Controller:
     # Initialise the PCA9685 using the default address (0x40).
