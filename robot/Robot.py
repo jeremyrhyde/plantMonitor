1a0c2bd8ceb461bd10fd5cacdb1dbee561c33e81
Jeremy Hyde <jeremy.hyde@latch.com>
2021-08-30 22:46:57 -0400

    push with move to points (non threaded)

--- robot/Robot.py
+++ robot/Robot.py
@@ -324,7 +324,8 @@ class Robot:
             dt = 0
             i = 0
             while dt < float(water_info['amount']):
-                self.cnc.move_points(water_info['points'][i%len(water_info['points'])])
+                new_pos, a = self.convert_to_pos(water_info['points'][i%len(water_info['points'])
+                self.cnc.move_points(new_pos, a)
                 curr_time = datetime.datetime.now()
                 dt = (curr_time - start_time).seconds + (curr_time - start_time).microseconds*0.000001
                 i = i+1
